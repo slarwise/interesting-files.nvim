17cfb198325ade68105f7623284f651492e369b3
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2021-12-08 22:46:45 +0100

    Fix inconsistent indentation

--- lua/telescope/_extensions/interesting_files.lua
+++ lua/telescope/_extensions/interesting_files.lua
@@ -1,6 +1,6 @@
 local has_telescope, telescope = pcall(require, 'telescope')
 if not has_telescope then
-  error('This plugins requires nvim-telescope/telescope.nvim')
+    error('This plugins requires nvim-telescope/telescope.nvim')
 end
 
 local pickers = require "telescope.pickers"
